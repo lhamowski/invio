43b4a56c66c1fb8b17ee76f096baee162405575f
Lukasz Hamowski <lukaszhamowski@gmail.com>
2020-04-20 22:44:12 +0200

    Add unicode compilation flags

--- cmake/toolchain/msvc.cmake
+++ cmake/toolchain/msvc.cmake
@@ -3,16 +3,22 @@ include_guard()
 set(Boost_USE_STATIC_LIBS ON)
 list(APPEND CMAKE_PREFIX_PATH $ENV{NU_SDK}/boost/1_72_0)
 
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 set(CMAKE_C_FLAGS 
-    "/DWIN32_LEAN_AND_MEAN /W4" 
+    "/DWIN32_LEAN_AND_MEAN"
+    "/D_UNICODE"
+    "/DUNICODE"
+    "/W4" 
     CACHE 
     STRING "Flags used by C compiler")
 
 set(CMAKE_CXX_FLAGS 
-    "/DWIN32_LEAN_AND_MEAN /W4" 
+    "/DWIN32_LEAN_AND_MEAN"
+    "/D_UNICODE"
+    "/DUNICODE"
+    "/W4" 
     CACHE 
     STRING "Flags used by C++ compiler")
