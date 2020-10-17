59362d0a4b40f02a8f4a0c0e23e1bec5e284935d
lhamowski <lukaszhamowski@gmail.com>
2020-10-17 14:35:19 +0200

    Add appveyor.yml for CI

--- src/config.hpp
+++ src/config.hpp
@@ -1,18 +1,12 @@
 #pragma once
 
-#include "core/logger_manager.hpp"
-
-#include <kl/reflect_struct.hpp>
-
 #include <string>
 
 namespace invio {
 
 struct config
 {
-    log_config log_cfg{};
 };
-KL_REFLECT_STRUCT(config, log_cfg)
 
 config load_config(const std::string& filepath);
 
